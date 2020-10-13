3a529255ea5e6497f9173f6c1953531e1375764e
Paulo <paulodigitalart@gmail.com>
2020-10-13 07:37:46 -0300

    Front-end

---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import { Link } from 'react-router-dom';
+
+import '../style/landing.css';
+import LogoImg from '../img/logo.png';
+
+
+function Landing(){
+    return (
+        <div id="page-landing">
+      
+
+        <div className="content-wrapper">
+          <img src={LogoImg} alt="Happy"/>
+  
+          <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia
+          de muitas crianças.</p>
+          </main>
+          
+          <div className="location">
+            <strong>São Paulo</strong>
+            <span>SP</span>
+          </div>
+           
+            <Link 
+            to="/app"
+            className="enter-app">
+            
+            <FiArrowRight 
+            size={26} 
+            color="rgba(0, 0, 0, 0.6)" 
+            />
+            
+            </Link>
+  
+  
+        </div>
+      </div>
+    );
+}
+
+export default Landing;
